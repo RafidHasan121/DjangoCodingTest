cae4b4daba867e32c03864b55203fe7c60f5277a
Rafid Hasan <rafidhasan121@gmail.com>
2022-07-16 16:20:36 +0600

    npm files created

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('src/templates/assets/js/app.js', 'src/static/js')
-    .sass('src/templates/assets/scss/main.scss', 'src/static/css');
+ mix.js('resources/js/app.js', 'public/js')
+ .react()
+ .sass('resources/sass/app.scss', 'public/css', [
+ //
+ ]);
